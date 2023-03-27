272f17b3640911223195c5ec4beb028f7773d580
Peter Prendergast <peterdunmain@outlook.com>
2023-03-28 00:31:48 +0100

    Constant refresh

--- booking.py
+++ booking.py
@@ -4,9 +4,13 @@ import pause
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
 driver.get('https://www.maynoothuniversity.ie/student-residences/bookings')
 logging.info('Chromedriver opened successfully')
@@ -16,14 +20,24 @@ driver.maximize_window()
 allow_cookies = driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
 allow_cookies.click()
 time.sleep(1)
+
 # Wait until the booking time
-pause.until(datetime(2023, 3, 27, 12, 49, 0))
+pause.until(datetime(2023, 3, 27, 21, 53, 0))
 logging.info('Booking time reached')
+while True:
+    try:
+        driver.refresh()
+        # Find the button which reads "Book Now"
+        book_now = driver.find_element(By.LINK_TEXT, "Continuing Students")
+        book_now.click()
+        logging.info('Clicked on Book Now')
+        break
+    except NoSuchElementException as e:
+        logging.info('Booking not available yet')
 # Refresh the page
-driver.refresh()
-logging.info('Page refreshed')
+# driver.refresh()
+# logging.info('Page refreshed')
 # Find the link which contains the text "Booking Events"
-booking_events = driver.find_element(By.LINK_TEXT, "Before You Book")
-booking_events.click()
-logging.info('Clicked on Book Now')
-time.sleep(500)
+# booking_events = driver.find_element(By.LINK_TEXT, "Before You Book")
+# booking_events.click()
+# logging.info('Clicked on Book Now')
