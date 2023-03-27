3029ae3f08dfa270d5f374159708766624be2e72
PeterLP123 <79646627+PeterLP123@users.noreply.github.com>
2023-03-27 21:05:42 +0100

    Further updates

--- booking.py
+++ booking.py
@@ -10,11 +10,20 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 
 driver.get('https://www.maynoothuniversity.ie/student-residences/bookings')
 logging.info('Chromedriver opened successfully')
+# Maximise the window
+driver.maximize_window()
+# Click on the button which reads "Allow all cookies"
+allow_cookies = driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+allow_cookies.click()
+time.sleep(1)
 # Wait until the booking time
 pause.until(datetime(2023, 3, 27, 12, 49, 0))
+logging.info('Booking time reached')
+# Refresh the page
 driver.refresh()
 logging.info('Page refreshed')
 # Find the link which contains the text "Booking Events"
-driver.find_element(By.LINK_TEXT, "Booking Events").click()
+booking_events = driver.find_element(By.LINK_TEXT, "Before You Book")
+booking_events.click()
 logging.info('Clicked on Book Now')
-time.sleep(5)
+time.sleep(500)
